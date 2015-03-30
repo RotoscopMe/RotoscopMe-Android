a4b2b6a68d5ef461a96e21cd86d76bf795779241
Matthieu-Riou <matthieu.riou@etu.univ-nantes.fr>
2015-03-30 15:26:20 +0200

    Add buttons !

--- src/src/fr/ihm/RotoscopMe/MainActivity.java
+++ src/src/fr/ihm/RotoscopMe/MainActivity.java
@@ -2,14 +2,25 @@ package fr.ihm.RotoscopMe;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
-    /**
-     * Called when the activity is first created.
-     */
+
+    protected Button createProjectButton;
+    protected Button openProjectButton;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+        createProjectButton = (Button) findViewById(R.id.create_project);
+        openProjectButton = (Button) findViewById(R.id.open_project);
+    }
+
+    public void createProject(View v)
+    {
+        createProjectButton.setText("Bravo !");
     }
 }
