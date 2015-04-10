81f122c86b611056275a064103a753b264b07b12
manon17 <manon.bobin@gmail.com>
2015-04-10 15:25:03 +0200

    Add Slide Menu

--- src/src/fr/ihm/RotoscopMe/DrawActivity.java
+++ src/src/fr/ihm/RotoscopMe/DrawActivity.java
@@ -1,13 +1,22 @@
 package fr.ihm.RotoscopMe;
 
 import android.app.Activity;
+import android.app.FragmentManager;
 import android.os.Bundle;
-import android.widget.Button;
-import android.widget.TextView;
+import android.support.v4.app.ActionBarDrawerToggle;
+import android.support.v4.app.Fragment;
+import android.support.v4.widget.DrawerLayout;
+import android.view.View;
+import android.widget.*;
+
 
 public class DrawActivity extends Activity {
 
     protected DrawZone drawzone;
+    private String[] mPlanetTitles;
+    private DrawerLayout mDrawerLayout;
+    private ListView mDrawerList;
+    private ActionBarDrawerToggle mDrawerToggle;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -15,5 +24,59 @@ public class DrawActivity extends Activity {
         setContentView(R.layout.draw);
 
         drawzone = (DrawZone) findViewById(R.id.drawzone);
+
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        mDrawerList = (ListView) findViewById(R.id.left_drawer);
+
+        // Set the adapter for the list view
+       // mDrawerList.setAdapter(new ArrayAdapter<String>(this,
+             //   R.layout.drawer_list_item, mPlanetTitles));
+        // Set the list's click listener
+        mDrawerList.setOnItemClickListener(new DrawerItemClickListener());
+
+        mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout,
+                R.drawable.ic_drawer, R.string.drawer_open, R.string.drawer_close) {
+
+            /** Called when a drawer has settled in a completely closed state. */
+            public void onDrawerClosed(View view) {
+                super.onDrawerClosed(view);;
+                invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
+            }
+
+            /** Called when a drawer has settled in a completely open state. */
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+                invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
+            }
+        };
+
+        // Set the drawer toggle as the DrawerListener
+        mDrawerLayout.setDrawerListener(mDrawerToggle);
     }
+    private class DrawerItemClickListener implements ListView.OnItemClickListener {
+        @Override
+        public void onItemClick(AdapterView parent, View view, int position, long id) {
+            selectItem(position);
+        }
+    }
+
+    /** Swaps fragments in the main content view */
+    private void selectItem(int position) {
+        // Create a new fragment and specify the planet to show based on position
+    //    Fragment fragment = new PlanetFragment();
+        Bundle args = new Bundle();
+      //  args.putInt(PlanetFragment.ARG_PLANET_NUMBER, position);
+       // fragment.setArguments(args);
+
+        // Insert the fragment by replacing any existing fragment
+        FragmentManager fragmentManager = getFragmentManager();
+       // fragmentManager.beginTransaction()
+         //       .replace(R.id.content_frame, fragment)
+           //     .commit();
+
+        // Highlight the selected item, update the title, and close the drawer
+        mDrawerList.setItemChecked(position, true);
+        mDrawerLayout.closeDrawer(mDrawerList);
+    }
+
 }
